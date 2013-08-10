c3cbacc853eae08bc1cdb6e70b52845296ab1572
Vladimir Markovnin <markovnin@gmail.com>
2013-08-10 20:49:17 +0400

    Отчет с рейтингом без учета квалификации

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -24,23 +24,23 @@ class ReportsController < ApplicationController
     faculties = Faculty.all.map(&:id).uniq
     years.each do |year|
 	  average_rating[year] ||= {}
-	  ratings = Point.where(year: year).joins(:employee).joins(:chair).map{|p| p.rating}
+	  ratings = Point.where(year: year).joins(:employee).joins(:chair).map{|p| [p.rating, p.inqualification_rating]}.transpose
 	  sum = sum(ratings)
 	  average_rating[year][:total] = sum if sum
       posts.each do |post|
 	  average_rating[year][post] ||= {}
-	  ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}).map{|p| p.rating}
+	  ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}).map{|p| [p.rating, p.inqualification_rating]}.transpose
 	  sum = sum(ratings)
 	  average_rating[year][post][:total] = sum if sum
 	faculties.each do |faculty|
-	  ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}, chairs: {faculty_id: faculty}).map{|p| p.rating}
+	  ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}, chairs: {faculty_id: faculty}).map{|p| [p.rating, p.inqualification_rating]}.transpose
 	  sum = sum(ratings)
 	  average_rating[year][post][faculty] = sum if sum
 	end
-    ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}, chairs: {clinic: true}).map{|p| p.rating}
+    ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}, chairs: {clinic: true}).map{|p| [p.rating, p.inqualification_rating]}.transpose
     sum = sum(ratings)
     average_rating[year][post][:clinical] = sum if sum
-    ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}, chairs: {clinic: nil}).map{|p| p.rating}
+    ratings = Point.where(year: year).joins(:employee).joins(:chair).where(employees: {post_id: post}, chairs: {clinic: nil}).map{|p| [p.rating, p.inqualification_rating]}.transpose
     sum = sum(ratings)
     average_rating[year][post][:teoretical] = sum if sum
       end
@@ -49,6 +49,10 @@ class ReportsController < ApplicationController
   end
   
   def sum(ratings)
-    sum = ratings.length > 0 ? "%.2f" % (ratings.sum/ratings.length.to_f) : nil
+    sum = []
+    ratings.each do |rating|
+      sum += rating.length > 0 ? ["%.2f" % (rating.sum/rating.length.to_f)] : [0]
+    end
+    sum
   end
 end
