75b5e0a7f5cb569a7b36619ed1af42f805bf510b
Vladimir Markovnin <markovnin@gmail.com>
2013-09-15 17:12:20 +0400

    Скрытие элементов редактирования для нередакторов

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    User.find_by_id(session[:user_id])
+  end
+  
+  def current_user_editor?
+    current_user.group.editor unless current_user.nil?
+  end
 end
