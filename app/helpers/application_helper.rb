736bc699584b4ca62a1d2a7b5c2c6a8c8e48ce9a
Vladimir Markovnin <markovnin@gmail.com>
2013-09-20 19:21:32 +0400

    Оптимизация выборки сотрудников

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def current_user
-    current_user = User.find_by_id(session[:user_id])
-  end
-  
-  def current_user_editor?
-    current_user.group.editor unless current_user.nil?
-  end
 end
--- app/models/point.rb
+++ app/models/point.rb
@@ -1,9 +1,14 @@
 class Point < ActiveRecord::Base
   belongs_to :employee
   has_one :chair, :through => :employee
+  has_one :post, :through => :employee
   
   def rating
     0.25 * self.qualification + 0.75 * (self.learning + self.science + self.clinic + self.social)
+  end
+  
+  def head_rating
+    rating + chair.rating[year]["rating"]/2 if employee.head
   end
   
   def inqualification_rating
